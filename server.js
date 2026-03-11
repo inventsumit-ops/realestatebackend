6430b7d7c7228c74e14d02c16da14f10efcfde01
xyz <xyz@example.com>
2026-03-11 15:29:50 +0530

    review  manager

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const limiter = rateLimit({
 app.use('/api/', limiter);
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://realestatebackend-nu.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
